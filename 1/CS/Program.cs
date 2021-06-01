636f6fe102bf3b9d51729c07d7b4fd1c86a35e91
BinbinQi <78098960@qq.com>
2021-06-01 15:34:56 +0800

    add c sharp code

---
+++ 1/CS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CS
+{
+    /// <summary>
+    /// Display a person's complete mailing address
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binbin Qi");
+            Console.WriteLine("College of Geophysics");
+            Console.WriteLine("China University of Petroleum");
+            Console.ReadKey();
+        }
+    }
+}
